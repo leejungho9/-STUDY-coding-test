28d359d331edb59f64c92d34313913e9732f57ed
jungho Lee <9wjdgh9@naver.com>
2022-11-06 22:33:22 +0900

    [level 1] Title: 평균 구하기, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.js
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.js
@@ -1,13 +1,13 @@
 function solution(arr) {
     
-    //결과 값을 반환하는 변수 answer 생성
+    //변수 answer 생성
     var answer = 0;
     
-    //매개변수인 배열 arr 의 요소를 다 더하기
+    // 매개변수인 배열 arr 의 요소를 다 더하기
     for( el of arr) {
         answer += el 
     }
     
-    // 평균값을 구하기 위해 배열의 길이만큼 나눈 값을 반환
+    // 배열의 길이만큼 나눈 값을 반환
     return answer/ arr.length;
 }

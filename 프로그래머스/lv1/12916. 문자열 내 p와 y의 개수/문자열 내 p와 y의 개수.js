f1c8d470eefa44c4f19f6d733fe80040a8e6c4d7
jungho Lee <9wjdgh9@naver.com>
2022-11-06 23:41:03 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
@@ -0,0 +1,28 @@
+function solution(s){
+    
+    var answer = true; // 변수 answer 생성
+    var p = 0; //p의 개수를 담고 있는 변수 생성
+    var y = 0; //y의 개수를 담고 있는 변수 생성
+    
+    // 매개변수의 s를 모두 소문자로 변경
+    s  = s.toLowerCase();
+    //반복문을 통해 p와 y의 개수를 세기
+    for(let i = 0; i < s.length; i++) {
+        if(s[i] === 'p') {
+            p++;
+        } else if(s[i] === 'y') {
+            y++;
+        }
+    }
+
+    //조건문으로 p와 y의 개수 비교
+    if(p === y) {
+    //p와 y의 개수가 같으면 True 반환        
+        answer = true
+    } else {
+    //p와 y의 개수가 다르면 False 반환
+        answer = false
+    }
+
+    return answer; //answer 반환
+}

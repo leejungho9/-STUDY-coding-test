0bc4f6cef33be6ceba402c8f91c2eb44e46defad
jungho Lee <9wjdgh9@naver.com>
2022-11-07 00:33:17 +0900

    [level 1] Title: 하샤드 수, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
@@ -5,8 +5,9 @@ function solution(x) {
     //x를 문자열로 변경
     x = String(x)
     
-    //반복문을 통해 x인덱스에 접근해 x의 자릿수의 합 구하기
+    //반복문을 통해 x인덱스에 접근
     for(let i = 0; i < x.length; i++) {
+        //다시 숫자 타입으로 변경 후 sum에 더하면서 대입 
         sum += Number(x[i]);
     }
 

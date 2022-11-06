6e97afedf16bdeaef9f09a634a50741a9f8ebebd
jungho Lee <9wjdgh9@naver.com>
2022-11-07 00:27:32 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -4,8 +4,8 @@ function solution(n) {
     var sqrt  = Math.sqrt(n);  // n의 제곱근을 담고있는 변수 sqrt 생성
    
     // 조건문으로 sqrt가 양의 정수의 제곱근인지 아닌지 확인 
-    // Number.isInteger입력받은 값이 정수인지 판단 정수면 true, 소수면 false 반환
-    if(sqrt > 0 && Number.isInteger(sqrt)) {
+    // Number.isInteger()로 입력받은 값이 정수인지 판단 정수면 true, 소수면 false 반환
+    if(Number.isInteger(sqrt)) {
         // 양의 정수 x의 제곱근이라면 x+1 리턴
         answer = Math.pow((sqrt+1), 2)
     } else {

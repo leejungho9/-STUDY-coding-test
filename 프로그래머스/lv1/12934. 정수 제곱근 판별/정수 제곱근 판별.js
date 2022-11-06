a755f409fc848543cd26a3b1f613be780ace498c
jungho Lee <9wjdgh9@naver.com>
2022-11-07 00:29:56 +0900

    Update 정수 제곱근 판별.js

--- 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -1,17 +1,14 @@
 function solution(n) {
-    
-    var answer = 0; // 변수 answer 생성
     var sqrt  = Math.sqrt(n);  // n의 제곱근을 담고있는 변수 sqrt 생성
    
     // 조건문으로 sqrt가 양의 정수의 제곱근인지 아닌지 확인 
     // Number.isInteger()로 입력받은 값이 정수인지 판단 정수면 true, 소수면 false 반환
     if(Number.isInteger(sqrt)) {
-        // 양의 정수 x의 제곱근이라면 x+1 리턴
-        answer = Math.pow((sqrt+1), 2)
+        // 양의 정수 x의 제곱근이라면 x+1의 제곱을 리턴
+        return Math.pow((sqrt+1), 2)
     } else {
          // 양의 정수 x의 제곱근이 아니라면 -1리턴
-        answer = -1;
+        return -1;
     }
     
-    return answer; //answer 반환
 }

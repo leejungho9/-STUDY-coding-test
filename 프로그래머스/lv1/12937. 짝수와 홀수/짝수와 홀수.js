920d74b2f29a966c5622d6decac5fd973345bc08
jungho Lee <9wjdgh9@naver.com>
2022-11-07 00:30:46 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12937. 짝수와 홀수/짝수와 홀수.js
+++ 프로그래머스/lv1/12937. 짝수와 홀수/짝수와 홀수.js
@@ -1,15 +1,12 @@
 function solution(num) {
     
-    var answer = '';  // 변수 answer 생성
-    
     //매개변수 num이 짝수라면 "Even" 반환
     if(num % 2 === 0) {
-        answer = "Even";
+        return "Even";
     } 
     //매개변수 num이 홀수라면 "Odd" 반환
     else {
-        answer = "Odd";
+        return "Odd";
     } 
     
-    return answer; //answer 반환
 }

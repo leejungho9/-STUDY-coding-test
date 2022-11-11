7c368f019a71e299bea9572f2f9d2920c477f6d1
jungho Lee <9wjdgh9@naver.com>
2022-11-11 13:07:59 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
+++ 프로그래머스/lv1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
@@ -1,5 +1,8 @@
 function solution(s) {
     var answer = '';
+    //s를 배열로 바꾸고 sort()으로 정렬
+    // reverse()를 사용해 내림차순으로 만듦
+    // 다시 join 으로 합쳐 문자열로 반환
     answer = s.split('').sort().reverse().join("");
     return answer;
 }

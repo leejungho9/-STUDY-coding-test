0f95944e1599c3ced19c5bb3ead627a3863c1faf
jungho Lee <9wjdgh9@naver.com>
2022-11-17 01:55:41 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 6.15 ms, Memory: 37.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -0,0 +1,13 @@
+function solution(strings, n) {
+
+    
+   strings.sort((a,b) => {
+       console.log(a,b)
+       if(a[n] < b[n])  return -1
+       if (a[n] === b[n]) return a < b ? -1 : 1;
+   })
+   
+   
+    
+    return strings;
+}

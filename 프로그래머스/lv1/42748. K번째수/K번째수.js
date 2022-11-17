0133b39d30dbb58b53c6c098887af2c2c7fea129
jungho Lee <9wjdgh9@naver.com>
2022-11-17 22:02:09 +0900

    [level 1] Title: K번째수, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.js
+++ 프로그래머스/lv1/42748. K번째수/K번째수.js
@@ -1,15 +1,15 @@
 function solution(array, commands) {
     var answer = [];
     
-    commands.map((command, i) => {
-     
-       let sliceArr = array.slice(command[0]-1, command[1])
-       let sortArr = sliceArr.sort((a, b) => a - b)
-   
-      answer.push(sliceArr[command[2]-1])
-       
-
+    //commands 안에 배열에 접근하기 위해 map 사용 후 반복
+    commands.map((command) => {
         
+       // array.slice 메서드로 commands[0] - command[1]범위만큼 잘라주고 결과를 sort로 오름차순 정렬함  
+        let sliceArr = array.slice(command[0]-1, command[1])
+        sliceArr.sort((a, b) => a - b)
+        //위에 정렬한 배열을 command[2]-1 범위를 answer에 할당
+        answer.push(sliceArr[command[2]-1])
+
     })
     return answer;
 }

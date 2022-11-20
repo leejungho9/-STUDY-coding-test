6471f9d0af162fb05041e10af6b4b833a2b6dd45
jungho Lee <9wjdgh9@naver.com>
2022-11-20 17:24:12 +0900

    [level 1] Title: 2016년, Time: 0.15 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.js
+++ 프로그래머스/lv1/12901. 2016년/2016년.js
@@ -1,14 +1,10 @@
 function solution(a, b) {
     var answer = '';
+    //요일을 담고 있는 배열 생성
+    //매개변수로 들어온 날짜형식을 변수에 할당
+    //getDay()메서드를 사용해 해당 요일의 인덱스 값 출력
+    //getDay()메서드는 현지 시간에 따라 지정된 날짜의 요일을 반환합니다. 여기서 0은 일요일 1은 월요일 ... (0-6)
     let day = ['SUN','MON','TUE','WED','THU','FRI','SAT']
-    // getDay가 반환하는 요일 값의 범위는 일요일(0) 부터 토요일(6) 입니다.
-    
-
     let date = `2016-${a}-${b}`
-    
-
-    answer = day[new Date(date).getDay()];
-    
-  
-    return answer;
+    return answer = day[new Date(date).getDay()];
 }

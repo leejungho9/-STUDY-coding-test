8c9061dc08a6a504f77fd9d6041a47cf9600b25b
jungho Lee <9wjdgh9@naver.com>
2022-11-21 12:58:33 +0900

    [level 1] Title: 폰켓몬, Time: 0.68 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
@@ -5,22 +5,33 @@ function solution(nums) {
     // n 마리의 포켓몬 중에서 n/2 마리를 가져갈 수 있음
     // 4마리의 포켓몬 종류 번호가 [3,1,2,3] 이라면 
     // 최대한 많은 종류의 포켓몬을 선택해야함
-    let mySet = new Set();
-    let arr = [];
     
+    
+    // set객체 생성
+    let mySet = new Set();
+    
+    //nums 돌면서 중복 없애기
     nums.map((num) => {
         mySet.add(num) 
     })
     
+    //배열에 담아줌
+    let myarr = [...mySet];
     
-    mySet.forEach((obj)=> {
-        arr.push(obj)
-    })
-    
-    if(nums.length /2 >= arr.length) {
-        return arr.length;
+    // nums배열의 길이를 2로 나눈 값보다 myarr의 길이가 크거나 같다면
+ 
+    if(nums.length /2 > myarr.length) {
+        return myarr.length;
     }   
-        return nums.length /2;
+        return nums.length / 2;
     
 }
+    //[3,1,2,3] => [3,1,2] 
+    //[3,3,3,2,2,4] => [3,2,1]
+    //[3,3,3,2,2,2] => [3,2]
 
+    // [3,1,2,3] ==== myarr : [3,1,2]
+    // nums.length 2 >= myarr.length 3
+    
+    // [3,3,3,2,2,2] ==== myarr : [3, 2 ]
+    // nums.length 2 >= myarr.length 3

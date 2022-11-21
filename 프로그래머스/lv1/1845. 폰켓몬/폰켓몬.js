23207d0d53dcaef1d7a61812a971e35e7f7bda36
jungho Lee <9wjdgh9@naver.com>
2022-11-21 14:17:46 +0900

    [level 1] Title: 폰켓몬, Time: 0.31 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
@@ -1,37 +1,22 @@
 function solution(nums) {
-    var answer = 0;
+
     
     
-    // n 마리의 포켓몬 중에서 n/2 마리를 가져갈 수 있음
-    // 4마리의 포켓몬 종류 번호가 [3,1,2,3] 이라면 
-    // 최대한 많은 종류의 포켓몬을 선택해야함
+    // 문제를 제대로 이해하지는 못했지만, 입출력 예시를 봤을 때 
+    // 1번 케이스를 제외하고 중복값을 제외한 후 배열의 길이가 result 인 것을 확인했음
+    // 1번 같이 예외인 케이스만 조건주면 될 듯 ? 
     
     
-    // set객체 생성
-    let mySet = new Set();
-    
-    //nums 돌면서 중복 없애기
-    nums.map((num) => {
-        mySet.add(num) 
-    })
-    
-    //배열에 담아줌
+    // set객체로 nums의 중복값 없애고 myarr 이라는 새로운 배열에 담기
+    let mySet = new Set(nums);
     let myarr = [...mySet];
     
-    // nums배열의 길이를 2로 나눈 값보다 myarr의 길이가 크거나 같다면
- 
-    if(nums.length /2 > myarr.length) {
-        return myarr.length;
-    }   
+    // nums 길이를 2로 나눈 값보다 중복값 제외한 myarr의 길이가 더 크다면? (1번 케이스)
+
+    if(nums.length / 2 <  myarr.length) {
         return nums.length / 2;
+    }   
+        return myarr.length;
     
 }
-    //[3,1,2,3] => [3,1,2] 
-    //[3,3,3,2,2,4] => [3,2,1]
-    //[3,3,3,2,2,2] => [3,2]
 
-    // [3,1,2,3] ==== myarr : [3,1,2]
-    // nums.length 2 >= myarr.length 3
-    
-    // [3,3,3,2,2,2] ==== myarr : [3, 2 ]
-    // nums.length 2 >= myarr.length 3

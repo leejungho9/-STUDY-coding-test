e0c922ce73d65df66adf3bcd9e437e1030deb496
jungho Lee <9wjdgh9@naver.com>
2022-11-28 12:52:55 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -11,21 +11,20 @@ function solution(lottos, win_nums) {
     
     let answer = [];
   
-    let wins = lottos.filter(lotto => win_nums.includes(lotto)).length;
-    let zero = lottos.filter(lotto => lotto === 0).length;
+    let wins = lottos.filter(lotto => win_nums.includes(lotto)).length; // 맞춘 개수 
+    let zero = lottos.filter(lotto => lotto === 0).length;              // 0의 개수
      
-    let Max = 7 - (wins + zero);  //최대로 맞춘 등수 
-    let Min = 7 - wins;         //최소로 맞춘 등수
-    
-    console.log(wins, zero)
+    let Max = 7 - (wins + zero);  //최대로 맞춘 등수  ==> 지금 현재 복권에서 당첨된 수 + 0의 개수
+    let Min = 7 - wins;           //최소로 맞춘 등수  ==> 지금 현재 복권에서 당첨된 수
+     
+ 
     if(wins === 0 && zero === 0) { //맞춘 번호도 , 0도 없는 경우
-        Max = 6;
+        Max = 6;  
         Min = 6;
-    }else if(wins === 0) {  //맞춘 번호가 없는 경우 
+    }else if(wins === 0) {  //맞춘 번호만 없는 경우 
         Min = 6;
     }
 
-    
 
     answer.push(Max, Min); 
     

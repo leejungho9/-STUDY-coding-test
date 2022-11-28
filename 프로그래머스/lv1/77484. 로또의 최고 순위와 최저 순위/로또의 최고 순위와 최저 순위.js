da2dd0c374bbfdf36fb2b0e7773accbd7638ebb7
jungho Lee <9wjdgh9@naver.com>
2022-11-28 12:49:45 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 2.40 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -0,0 +1,35 @@
+function solution(lottos, win_nums) {
+
+    //1부터 45까지의 숫자 중 6개를 찍어서 맞히는 대표적인 복권입니다.
+    //알아볼 수 없는 번호를 0으로 표기하기로 하고
+    //민우는 자신이 구매했던 로또로 당첨이 가능했던 최고 순위와 최저 순위를 알아보고 싶어 졌습니다.
+  
+    
+    
+    //최고 순위 지금 현재 복권에서 당첨된 수 + 0의 개수
+    //최저 순위 지금 현재 복권에서 당첨된 수 || 0만 있다면 0의 개수
+    
+    let answer = [];
+  
+    let wins = lottos.filter(lotto => win_nums.includes(lotto)).length;
+    let zero = lottos.filter(lotto => lotto === 0).length;
+     
+    let Max = 7 - (wins + zero);  //최대로 맞춘 등수 
+    let Min = 7 - wins;         //최소로 맞춘 등수
+    
+    console.log(wins, zero)
+    if(wins === 0 && zero === 0) { //맞춘 번호도 , 0도 없는 경우
+        Max = 6;
+        Min = 6;
+    }else if(wins === 0) {  //맞춘 번호가 없는 경우 
+        Min = 6;
+    }
+
+    
+
+    answer.push(Max, Min); 
+    
+    
+    return answer;
+}
+

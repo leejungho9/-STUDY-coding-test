f80ea5fff3731523941388ebde1acfed196b6548
jungho Lee <9wjdgh9@naver.com>
2022-11-28 13:15:05 +0900

    [level 1] Title: [1차] 다트 게임, Time: 3.13 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
@@ -1,105 +1,98 @@
 function solution(dartResult) {
-    let answer = [];
-    let arr = [[],[],[]];
-    let str =  dartResult.split(/[0-9]/g).join(",") //숫자를 다 , 로 바꾼 문자열  ===> ,D,S,T
-   
-    let score = dartResult.replace(/[a-zA-Z#*]/g , ',') // 문자,*,# 을 다 , 로 바꾼 문자열  ===> 1,2,,3,
-    score = score.replace(',,',',').split(",");                   // 1,2,,3, 연속된 쉼표를 하나의 쉼표로 바꿔줌 1,2,3,
-    console.log(score)
-    const sc = score.filter((s) => {
-        return s !== ''
-    })
+    let arr = [[],[],[]];   // [[1,S],[2,D*],[3T]]
     
-    let NumberScore = sc.map((el) => {
-        return Number(el)
-    })
-    console.log(NumberScore);
-    const myarr =  [];
-    // ,S,D*,T
-    for(let i = 0; i< str.length; i++) {
-        // i = 0 ? 
-        if(str[i] !== ',') {
-            if(str[i] === '*' ) {
-                myarr.pop()
-                myarr.push(str[i-1]+'*')
-            }else if(str[i] === '#') {
-                myarr.pop()
-                myarr.push(str[i-1]+'#')
-            }else {
-                     myarr.push(str[i])
-                
-            }
-        } 
-    }
-    console.log(myarr)
+    let str =  dartResult.split(/[0-9]/g).join(",").split(","); //숫자를 다 , 로 바꾼 문자열  ===> ,D,S,T
+    let score = dartResult.replace(/[a-zA-Z#*]/g , ',').split(",") // 문자,*,# 을 다 , 로 바꾸고 쉼표로 자르기 ===> 	[ '1', '2', '3', '', '' ]
 
-    myarr.map((el, idx) => {
+    let Bonus =  RemoveEmpty(str) //빈문자열 제거하고 배열로 리턴하는 함수 실행  ===> 	[ 'D', 'S', 'T*' ]
+    let NumberScore =  RemoveEmpty(score, 'number') //빈문자열 제거, 숫자타입으로 바꾼 배열을 리턴하는 함수 실행 ===> [ 1, 2, 3 ]
+    
+    
+    Bonus.map((el, idx) => { // [[1,S],[2,D*],[3T]] 형태 만들어주기
         arr[idx] = ([NumberScore[idx], el]);
     })
     
-    // [[1,S],[2,D*],[3T]]
-    console.log(arr)
     
-    // Single(S), Double(D), Triple(T) 영역이 존재하고 각 영역 당첨 시 
-    // 점수에서  1제곱, 2제곱, 3제곱 (점수1 , 점수2 , 점수3 )으로 계산된다.  
+    // Single(S), Double(D), Triple(T) 영역이 존재하고 각 영역 당첨 시 점수에서  1제곱, 2제곱, 3제곱 (점수1 , 점수2 , 점수3 )으로 계산된다.  
     
-    //점수계산
-    //[1,S]
+    // 점수계산
+    
+    // [[1,S],[2,D*],[3T]] 형태 만들어주기
+    let answer = [[],[],[]]; // 각 점수가 들어갈 곳 ===> 예시 [ 1번 기회 점수,  2번 기회 점수, 3번 기회 점수 ]
     for(let i = 0; i < arr.length; i++) {
-        if(arr[i][1].length === 1) {  
-          if(arr[i][1] ==='S') {
-             answer[i] = Math.pow(arr[i][0], 1)
-          } else if(arr[i][1] ==='D') {
-             answer[i] = Math.pow(arr[i][0], 2)
-          } else if(arr[i][1] ==='T') {
-             answer[i] = Math.pow(arr[i][0], 3)
-          }
-       
-        }else {
-          if(arr[i][1][1] === '#') {
-
+        if(arr[i][1].length === 1) {  //첫번째 기회에 *과 #이 없는 경우 단순 보너스 계산 실행
+            if(arr[i][1] ==='S') {
+                answer[i] = Math.pow(arr[i][0], 1)
+            } else if(arr[i][1] ==='D') {
+                answer[i] = Math.pow(arr[i][0], 2)
+            } else if(arr[i][1] ==='T') {
+                answer[i] = Math.pow(arr[i][0], 3)
+            } 
+        }if(arr[i][1][1] === '*') { // *이 있는 경우
+            if(arr[i][1].length !== 1) { //첫 번째 순서에 * 이 있는 경우
+                if(arr[i][1][0] === 'S') {
+                    answer[i-1] *=  2
+                    answer[i] = Math.pow(arr[i][0], 1) * 2
+                } else if(arr[i][1][0] === 'D') {
+                    console.log(answer[i-1]);
+                    answer[i-1] *= 2
+                    answer[i] = Math.pow(arr[i][0], 2) * 2
+                }else if(arr[i][1][0]  === 'T') {
+                    answer[i-1] *= 2
+                    answer[i] = Math.pow(arr[i][0], 3) * 2
+                }
+                // [[1,S],[2,D*],[3,T]]
+            } else if(arr[i][1].length === 1) { //두, 세 번째순서에 * 이 있는 경우
+                if(arr[i][1][0] === 'S') {
+                    answer[i-1] *=  2
+                    answer[i] = Math.pow(arr[i][0], 1) * 2
+                } else if(arr[i][1][0] === 'D') {
+                    console.log(answer[i-1]);
+                    answer[i-1] *= 2
+                    answer[i] = Math.pow(arr[i][0], 2) * 2
+                }else if(arr[i][1][0]  === 'T') {
+                    answer[i-1] *= 2
+                    answer[i] = Math.pow(arr[i][0], 3) * 2
+                }
+            } 
+        }if(arr[i][1][1] === '#') { //첫번째 기회에 #이 있는 경우
             if(arr[i][1][0] === 'S') {
                 answer[i] = Math.pow(arr[i][0], 1) * -1
-                  console.log( Math.pow(arr[i][0], 1) * -1)
-             }else if(arr[i][1][0] === 'D') {
+                console.log( Math.pow(arr[i][0], 1) * -1)
+            } else if(arr[i][1][0] === 'D') {
                 answer[i] = Math.pow(arr[i][0], 2) * -1
-                  console.log(answer[i])
-             }else if(arr[i][1][0]  === 'T') {
+                console.log(answer[i])
+            } else if(arr[i][1][0]  === 'T') {
                 answer[i] = Math.pow(arr[i][0], 3) * -1
-             }       
-          } else if(arr[i][1][1] === '*'){
-             if(i !== 0) {
-                 if(arr[i][1][0] === 'S') {
-                      answer[i-1] *=  2
-                     answer[i] = Math.pow(arr[i][0], 1) * 2
-                 }else if(arr[i][1][0] === 'D') {
-                     console.log(answer[i-1]);
-                     answer[i-1] *= 2
-                     answer[i] = Math.pow(arr[i][0], 2) * 2
-                 }else if(arr[i][1][0]  === 'T') {
-                      answer[i-1] *= 2
-                     answer[i] = Math.pow(arr[i][0], 3) * 2
-                 }
-             } else {
-                  if(arr[i][1][0]  === 'S') {
-                     answer[i] = Math.pow(arr[i][0], 1) * 2
-                 }else if(arr[i][1][0]  === 'D') {
-                      answer[i] = Math.pow(arr[i][0], 2) * 2
-                 }else if(arr[i][1][0]  === 'T') {
-                      answer[i] = Math.pow(arr[i][0], 3) * 2
-                 }
-             } 
-          }
-        }
+            }       
+         } 
+            
+        
     }
 
-
+    console.log(answer) 
+    // [ 1번 기회 점수,  2번 기회 점수, 3번 기회 점수 ] 다 더해서 return 
     return answer.reduce((acc, cur) =>{
         return acc+cur
     })
     
         
 }
+
+//공백 제거 후 필요에 따라 number 타입으로 변경하는 함수 
+function RemoveEmpty (arr , option) {
+    if(option === 'number') {
+       let result = arr.filter((el) => el !== '')     
+       return result.map((el) => {
+             return Number(el)
+        })
+    } else {
+        return arr.filter((el) =>  el !== '')
+    }
+}
+
+
+
 
 
 //다트 게임은 다트판에 다트를 세 차례 던져 그 점수의 합계로 실력을 겨루는 게임으로, 

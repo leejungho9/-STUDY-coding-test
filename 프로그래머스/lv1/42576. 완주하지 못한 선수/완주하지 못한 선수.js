0ab29376f0ffedcc626e4b06035a364ebd75c06b
jungho Lee <9wjdgh9@naver.com>
2022-11-30 01:51:24 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 92.65 ms, Memory: 57 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,11 +1,14 @@
 function solution(participant, completion) {
     var answer = ' ';
-    completion.sort();
-    participant.sort();
     
-     for(var i=0;i<participant.length;i++){
+    //문자열 정렬이니까 매개변수 생략, sort()
+    completion.sort();  
+    participant.sort();
+
+    //마라톤 참여한 선수 길이만큼 반복 후 완주한 선수의 배열과 비교해서 다르면 return
+     for(let i = 0; i < participant.length;i++) {
         if(participant[i] !== completion[i]){
             return participant[i];
         }
-    }
+     }
 }

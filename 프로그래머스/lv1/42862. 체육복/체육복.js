ec4b44be8fef4a040364268ba548f119f4fdfa9b
jungho Lee <9wjdgh9@naver.com>
2022-11-30 18:47:29 +0900

    [level 1] Title: 체육복, Time: 3.25 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.js
+++ 프로그래머스/lv1/42862. 체육복/체육복.js
@@ -3,7 +3,7 @@ function solution(n, lost, reserve) {
     let possible =  n - lost.length     // 체육 참여가 가능한 사람의 수 (전체 학생수 - 체육복 잃어버린 사람의 수)
     
     let realLost = [];
-    //만약 잃어버린 사람 중에 여분의 체육복이 있는 사람이라면
+    // 여분의 체육복이 있는 사람이 체육복을 도난당했다면..
     for(let i = 0; i < lost.length;  i++) {
         if(reserve.includes(lost[i])){  
             possible++;
@@ -12,13 +12,15 @@ function solution(n, lost, reserve) {
             realLost.push(lost[i]); //여분이 없는 사람 ==== 진짜 잃어비린 사람
         }
     }
+    
+    console.log(realLost) //〉	lost =  [6, 5, 4, 2] reserve =  [2, 4]  realLost ===[ 6, 5 ]
+
     //테스트  13, 14 줄만 에러나서 검색해보니 
-    // lost와 reserve가 정렬되지 않은 배열이라고 정렬해주란다,...
+    //lost와 reserve가 정렬되지 않은 배열이라고 정렬하란다..
     realLost.sort((a,b) => a-b)
-    reserve.sort((a,b) => a-b)
     
     //진짜 잃어버린 사람의 배열로 조건 돌리기
-     for(let i = 0; i < lost.length;  i++) {
+     for(let i = 0; i < realLost.length;  i++) {
         if(reserve.includes(realLost[i]-1)) {
             possible++;
             reserve = reserve.filter((re) =>  re !== realLost[i]-1)

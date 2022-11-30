dad5b317c30a221dff721a7438193ad33e60709e
jungho Lee <9wjdgh9@naver.com>
2022-11-30 15:40:26 +0900

    [level 1] Title: 체육복, Time: 3.13 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42862. 체육복/체육복.js
@@ -0,0 +1,49 @@
+function solution(n, lost, reserve) {
+
+
+    // n : 전체 학생의 수 
+    // lost : 체육복 도난 당한 학생의 번호
+    // reverse : 여벌의 체육복을 가져온 학생
+    
+    let lostStudent = lost.length;      // 체육복 잃어버린 사람의 수 
+    let possible =  n - lostStudent     // 체육 참여가 가능한 사람의 수 (전체 학생수 - 체육복 잃어버린 사람의 수)
+    let realLost = [];
+
+
+    //만약 잃어버린 사람 중에 여분의 체육복이 있는 사람이라면
+
+    
+    
+    
+    
+
+    for(let i = 0; i < lostStudent;  i++) {
+        if(reserve.includes(lost[i])){
+            possible++;
+            reserve = reserve.filter((re) =>  re !== lost[i])
+        } else {
+            realLost.push(lost[i]); //진짜 잃어비린 사람
+        }
+    }
+
+
+    lost = realLost.sort(( a,b ) => a-b)
+    console.log(lost)
+    
+
+    
+     for(let i = 0; i < lostStudent;  i++) {
+        if(reserve.includes(lost[i]-1)) {
+            possible++;
+            reserve = reserve.filter((re) =>  re !== lost[i]-1)
+         }else if(reserve.includes(lost[i]+1)) {
+            possible++;
+            reserve = reserve.filter((re) =>  re !== lost[i]+1)    
+        }
+    }
+    
+    
+   
+    return possible
+    
+}

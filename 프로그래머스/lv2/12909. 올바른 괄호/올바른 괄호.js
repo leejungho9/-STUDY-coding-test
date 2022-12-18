ddae73199c317714b07c322a113f960d347304ea
jungho Lee <9wjdgh9@naver.com>
2022-12-19 03:21:46 +0900

    [level 2] Title: 올바른 괄호, Time: 5.39 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,17 +1,24 @@
 
 function solution(s) {
-  //count로 ( 왼쪽괄호면 +1 ) 오른쪽 괄호면 -1 해주기
-  //괄호가 바르게 짝지어졌다면 count는 0이 될거임 
-  let count = 0;
-    s = s.split("")
-  for (let i of s) {  
-    if (i === "(") {  count += 1 };
-    if (i === ")") {  count -= 1 };
-    if (count < 0) {
+    //count로 ( 왼쪽괄호면 +1 ) 오른쪽 괄호면 -1 해주기
+    //괄호가 바르게 짝지어졌다면 count는 0이 될거임 
+    let count = 0;
+     
+
+    for(let el of s) {
+        if(el === "(") {
+            count += 1;
+        }
+        if(el === ')'){
+            count -= 1;
+        }
+          if (count < 0) {
         return false
     }
+    }
+    
+  
+    return count === 0;
   }
-
-  return count === 0;
-}
+  
 

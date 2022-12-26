baff846641aedaf6663cc9d54b37b983d2abcb76
jungho Lee <9wjdgh9@naver.com>
2022-12-26 13:03:51 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.19 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,17 +1,17 @@
 function solution(n, words) {
     let answer = [0,0];
-    const checkArr = [words[0]];
+    const checkArr = [words[0]]; // 첫번째 단어를 미리 넣어둠
 
     for (let i = 1; i < words.length; i++) {    
-        let word = words[i-1]   //이전 단어
-        let curword = words[i]    //비교할 단어   
+        let preWord = words[i-1]   //이전 단어
+        let word = words[i]    //비교할 단어   
     
-        // 첫 글자의 마지막 글자와, 두 번째 글자의 첫번째 글자가 같고  동일한 글자가 없는지 확인
-        if(word.charAt(word.length-1) === curword.charAt(0) && checkArr.indexOf(curword) === -1) {
-            checkArr.push(curword)
+        // 이전 단어의 마지막 글자와, 비교할 단어의 첫번째 글자가 같고,  동일한 글자가 없었는지 확인
+        if(preWord.charAt(preWord.length-1) === word.charAt(0) && checkArr.indexOf(word) === -1) {
+            checkArr.push(word)
         }else {
             // 가장 먼저 탈락하는 사람의 번호, 그 사람이 자신의 몇 번째에 탈락하는 지
-            let player = (i%n) +1 
+            let player = (i+1 % n)
             let round =  Math.floor(i / n) + 1
             return [(i%n) +1, round];
         }    

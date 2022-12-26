25b812ccd6e9f5c103e1e932f06b00ef44f10aec
jungho Lee <9wjdgh9@naver.com>
2022-12-26 12:44:15 +0900

    [level 2] Title: 구명보트, Time: 35.04 ms, Memory: 37.8 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.js
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.js
@@ -3,15 +3,25 @@ function solution(people, limit) {
     people.sort((a,b)=>b - a); //내림차순 [80, 70, 50, 50]
     
     let peopleLength = people.length;
-    //wp일 무거운사람과 제일 가벼운 사람을 조합
-
+    
+    //내림차순 [80, 70, 50, 50]
+    //               length
     for(let i = 0; i < peopleLength; i++){
 
-        //80 + 50 , 70 + 50 , 50 + 50
-        if(people[i]+people[peopleLength-1]<=limit){
+        //80 + 50 -> answer++ 
+        //70 + 50 -> answer++
+        //50 + 50 -> peoplelength-1  후 answer++ (두 명 보트에 태움)
+        //i와 peoplelength-1 의 길이가 같아져 반복문이 종료 ..
+        
+        //제일 무거운사람과 제일 가벼운 사람을 조합해서 보트에 타지면 people--
+        if(people[i] + people[peopleLength-1] <= limit){
             peopleLength--;
         }
+        
+        //limit을 넘어가면 그냥 answer++하기
         answer++;
     }
     return answer;
 }
+
+

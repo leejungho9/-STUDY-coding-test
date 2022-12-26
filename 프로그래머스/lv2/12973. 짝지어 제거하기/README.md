0ee774d8feccceb45722d80f6048772effbf021d
jungho Lee <9wjdgh9@naver.com>
2022-12-26 12:59:23 +0900

    [level 2] Title: 짝지어 제거하기, Time: 34.94 ms, Memory: 58 MB -BaekjoonHub

--- 프로그래머스/lv2/12973. 짝지어 제거하기/README.md
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 짝지어 제거하기 - 12973 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12973#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12973) 
 
 ### 성능 요약
 
-메모리: 58 MB, 시간: 35.76 ms
+메모리: 58 MB, 시간: 34.94 ms
 
 ### 구분
 
--- 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -2,10 +2,12 @@ function solution(s){
     s = s.split("")
     const arr = [s[0]]; //arr 에 초기값으로 첫번째 알파벳 넣어주기
     for(let i= 1; i < s.length; i++){  
-        if(arr[arr.length-1] === s[i] ){ //arr마지막 값과 들어오는 알파벳 값 비교 
-            arr.pop();                   //같으면 arr에 들어있는 값을 지움
+        //예)  ['b']  === 'a' ====>  ['b', 'a'] 
+        //     ['a']  === 'a' ====>  ['b']
+        if(arr[arr.length-1] === s[i] ){ //arr마지막 값과 들어오는 알파벳 값 비교,  같으면 arr에 들어있는 값을 pop()으로 지움, 같지 않으면 push해서 알파벳 넣어줌
+            arr.pop();                   
         }else {                            
-            arr.push(s[i])               //같지 않으면 push해서 알파벳 넣어줌
+            arr.push(s[i])               
         }
     }
     return arr.length === 0 ? 1 : 0

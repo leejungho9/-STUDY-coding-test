8ac0133c21360cfbfe7a843d72d9a487c25a3031
jungho Lee <9wjdgh9@naver.com>
2023-01-02 17:34:44 +0900

    [level 2] Title: [1차] 캐시, Time: 0.54 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.js
+++ 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.js
@@ -1,15 +1,16 @@
 function solution(cacheSize, cities) {
-    var answer = 0;
-
-
+    let answer = 0;
+    //LRU 알고리즘은 가장 오랫동안 사용하지 않은 페이지를 교체하는 알고리즘임
+    //캐시의 사이즈가 0일 때
     if(cacheSize === 0) {
         return cities.length * 5;
     } 
+    
     const cache=[];
-   
-    //캐시에 있는 경우 :  캐시 배열에서 해당 값을 지우고 맨 뒤에 해당 값을 푸시
-    //[오래된수 ------ 최신순]
-    //캐시가 없는 경우 : 캐시 배열에서 맨 앞의 값 (가장 오래된 값을) 제거 후 하고 맨 뒤에 city값을 push해줍니다.
+    
+    //[오래된순 ------ 최신순]
+    //캐시가 있는 경우 :  캐시 배열에서 해당 값을 지우고 맨 뒤에 해당 값을 푸시
+    //캐시가 없는 경우 :  캐시 배열에서 맨 앞의 값 (가장 오래된 값을) 제거 후 하고 맨 뒤에 city값을 push해줍니다.
    
    
     for(let i = 0; i < cities.length; i++) {
@@ -20,7 +21,7 @@ function solution(cacheSize, cities) {
             cache.push(cities[i]) 
             answer += 1;
         }else {
-            cache
+
             if(cache.length === cacheSize) {
                 cache.shift();
             }

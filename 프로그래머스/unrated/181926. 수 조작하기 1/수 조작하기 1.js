38adf1558ee302d39e105756769002ce2828d648
이정호 <9wjdgh9@naver.com>
2023-06-28 13:17:00 +0900

    [unrated] Title: 수 조작하기 1, Time: 5.77 ms, Memory: 37.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.js
@@ -0,0 +1,19 @@
+function solution(n, control) {
+    var answer = n;
+    
+    for(el of control) {
+
+        if(el === 'w'){
+             answer += 1;
+        } else if(el === 's'){
+             answer -= 1;
+        }
+        else if(el === 'd'){
+            answer += 10;
+        }
+        else if(el === 'a'){
+             answer -= 10;
+        }
+    }
+    return answer;
+}
